8d314a14a0f66c021b2ac9913d51ad502251a14c
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-07 19:49:51 -0400

    created basic ui

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -1,5 +1,4 @@
 <script>
-	
 	let total = 0;
 	let pts_this_round = 0
 	let r1, r2, r3, r4, r5 = 0;
@@ -10,16 +9,28 @@
 		last_roll = r
 	}
 </script>
-
+<br><br>
 <main>
-	<div class="columns">
-		<div class="column">
-			<h1 class="title">Round scores</h1>
-		</div>
-		<div class="column">
-			<h1 class="title">Board</h1>
-			<h1 class="subtitle">Last roll: {last_roll}</h1>
-			<button class="button is-primary" on:click={roll}>Roll</button>
+	<div class="container">
+		<div class="columns">
+			<div class="column">
+				<div class="box">
+					<h1 class="title">Round scores</h1>
+				</div>
+			</div>
+			<div class="column">
+				<div class="box">
+					<h1 class="title">Board</h1>
+					<h1 class="subtitle">Total score: {total}</h1>
+					<h1 class="subtitle">Last roll: {last_roll}</h1>
+					<button class="button is-primary" on:click={roll}>Roll</button>
+				</div>
+			</div>
+			<div class="column">
+				<div class="box">
+					<h1 class="title">Current Round</h1>
+				</div>
+			</div>
 		</div>
 	</div>
 </main>
