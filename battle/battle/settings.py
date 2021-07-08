c4839bbbacbef4c1c6e78a17ec4b0ba2d3c45af9
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-08 11:45:48 -0400

    wow it works

--- battle/battle/settings.py
+++ battle/battle/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'main.apps.MainConfig',
-    'django_svelte'
+    'django_svelte',
+    'rest_framework'
 ]
 
 MIDDLEWARE = [
