e1dc8707ba3961127c6547b56acac3f7c735ac7a
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-08 17:11:13 -0400

    created view and html for leaderboard

--- battle/main/views.py
+++ battle/main/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
+from .models import Submission
 
 # Create your views here.
 def index(req):
     return render(req, "index.html")
+
+def leaderboard_view(req):
+    objs = Submission.objects.all()
+    return render(req, 'leaderboard.html', {'lst': objs})
