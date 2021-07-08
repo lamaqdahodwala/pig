d9f9659bff9a45817e86136e3f413487ff6ae956
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-08 11:31:47 -0400

    created new model

--- battle/main/models.py
+++ battle/main/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Submission(models.Model):
+    name = models.CharField(max_length=100)
+    total = models.IntegerField()
+    r1 = models.IntegerField()
+    r2 = models.IntegerField()
+    r3 = models.IntegerField()
+    r4 = models.IntegerField()
+    r5 = models.IntegerField()
