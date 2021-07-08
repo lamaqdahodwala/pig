f8cc98c12d7c774f4108cbf094c700635b518b99
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-08 11:11:24 -0400

    next round mechanism works

--- svelte/src/App.svelte
+++ svelte/src/App.svelte
@@ -3,10 +3,27 @@
 	let pts_this_round = 0
 	let r1, r2, r3, r4, r5 = 0;
 	let last_roll = 0;
+	let round = 1
+	
+	function end_game(){
+	
+	}
+
+	function next_round(){
+		total += pts_this_round
+		last_roll = 0
+		pts_this_round = 0
+		alert("next round")
+	}
 
 	function roll(){
 		let r = Math.ceil(Math.random() * 6)
+		if (r == 6){ 
+			pts_this_round = 0
+			return next_round()
+		}
 		last_roll = r
+		pts_this_round += r
 	}
 </script>
 <br><br>
@@ -20,10 +37,14 @@
 			</div>
 			<div class="column">
 				<div class="box">
-					<h1 class="title">Board</h1>
+					<h1 class="title">Round #{round}</h1>
 					<h1 class="subtitle">Total score: {total}</h1>
+					<h1 class="subtitle">Possible points: {total + pts_this_round}</h1>
 					<h1 class="subtitle">Last roll: {last_roll}</h1>
-					<button class="button is-primary" on:click={roll}>Roll</button>
+					<div class="is-flex is-justify-content-center is-flex-direction-row">
+						<button class="button is-primary mx-3" on:click={roll}>Roll</button>
+						<button class="button is-info mx-3" on:click={next_round}>End round</button>
+					</div>
 				</div>
 			</div>
 			<div class="column">
