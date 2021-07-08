7da19e2b8588b5b414b2900cac3dfd73a4c15b8c
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-08 11:53:14 -0400

    created new url route

--- battle/main/api/urls.py
+++ battle/main/api/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [ 
-    path('submit', views.SubmitGameView.as_view())
+    path('submit', views.SubmitGameView.as_view()),
+    path('all', views.GetAllSubmissionsView.as_view())
 ]
