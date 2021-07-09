3eb9abe3a9965fa62ac31d553a29419b1e112ef1
Lamaq Dahodwala <74932012+lamaqdahodwala@users.noreply.github.com>
2021-07-09 15:21:06 +0000

    forgot the build command

--- main.sh
+++ main.sh
@@ -1,5 +1,8 @@
 pip install -r requirements.txt
 cd svelte
-npm install 
+npm install
+npm run build
 cd ../battle
+python manage.py migrate
+python manage.py collectstatic --noinput
 gunicorn battle.wsgi --bind 0.0.0.0:3000
