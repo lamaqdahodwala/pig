42fe8dccbdbfe814320eaf63933f522737f3e2db
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-09 11:16:50 -0400

    added some more styles

--- battle/main/urls.py
+++ battle/main/urls.py
@@ -4,5 +4,6 @@ from django.urls import path
 urlpatterns = [
     path("game", views.game, name="game"),
     path("leaderboard", views.leaderboard_view, name="leaderboard"),
-    path('how_to_play', views.how_to_play, name='howtoplay')
+    path('how_to_play', views.how_to_play, name='howtoplay'),
+    path('', views.index, name='index')
 ]
