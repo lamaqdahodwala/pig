ee37f4f9b0af9dfb3c4ec79bec916e12bfeffafa
Lamaq Dahodwala <lamaq091008@gmail.com>
2021-07-08 20:48:59 -0400

    created how to play

--- battle/main/views.py
+++ battle/main/views.py
@@ -3,9 +3,12 @@ from .models import Submission
 
 # Create your views here.
 def game(req):
-    return render(req, "index.html")
+    return render(req, "game.html")
 
 
 def leaderboard_view(req):
     objs = Submission.objects.all().order_by("-total")
     return render(req, "leaderboard.html", {"lst": objs})
+
+def index(req):
+    return render(req, 'index.html')
